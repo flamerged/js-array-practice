92327b750288c8a5cd894589d4707cb352abc913
flamerged <mersad.ajanovic@googlemail.com>
2018-12-20 23:13:54 +0000

    create array practice done

--- 1_create_array.js
+++ 1_create_array.js
@@ -8,23 +8,37 @@ If your program is stuck in an infinite loop, you can break out of the program b
 */
 
 // 1. Create a new array named languages that contains at least 7 programming languages.
-
+var languages = ["JavaScript", "HTML", "CSS", "C", "C++", "C#", "Java", "Python", "Ruby", "PHP"];
 // 2. Use console.log to display the number of elements in the array:
 
 /* 3. Use console.log to list the first element from the array using an index value. */
+console.log(languages[0]);
 
 /* 4. Use console.log to list the last element from the array using an index value. */
+console.log(languages[languages.length-1]);
 
 // 5. Use an array method to add an element to the END of the array.
 
+languages.push("R");
+
 // 6. Use an array method to add an element to the BEGINNING of the array.
+
+languages.unshift("Matlab");
 
 /* 7. Log all of the elements in the array as a single string using the array .join() method */
 
+console.log(languages.join(", "));
+
 // 8. Use an array method to REMOVE the LAST element from the array.
+
+languages.pop();
 
 // 9. Use an array method to REMOVE the first element from the array.
 
+languages.shift();
+
 /* 10. Log all of the elements in the array as a single string using the array .join() method. Thisis the same as step 7 above */
+
+console.log(languages.join(", "));
 
 // Run your code by typing node 1_create_array.js in the console below
